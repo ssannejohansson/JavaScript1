656f0c8a7a7fcd93dc62e4bd78fd58dd4b5187d5
ssannejohansson <sannej0hansson@hotmail.com>
2025-03-18 08:34:33 +0100

    Finished assignment
    Finished assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+// Exercise 1 
+
+console.log("hello world");
+
+// Exercise 2
+
+let firstName = "Sanne"; 
+let age = 29;
+let favoriteColor = "black";
+
+console.log(`your name is ${firstName}`); 
+console.log(`your age is ${age}`); 
+console.log(`your favorite color is ${favoriteColor}`);
+
+// Exercise 3
+
+let classmates = 12;
+classmates = 12 + 5; 
+console.log(classmates);
+
+classmates = 12 - 3;
+console.log(classmates);
+classmates = 12 * 3;
+
+console.log(classmates); 
+classmates = 12 / 2;
+console.log(classmates); 
+
